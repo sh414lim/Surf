15e0eaf8cad0f84451b9e8dda33e918fdfff8700
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-10-24 22:58:36 +0900

    basic setting

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,12 +1,48 @@
-import React from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { styled } from '@mui/system';
+import ItemList from './ItemList';
+
+const MyBox = styled(Box)({
+  display: 'flex',
+  justifyContent:'center',
+  marginTop:"50px",
+  });
+  const MyTabPanel= styled(TabPanel)({
+  })
+  const MyTabList = styled(TabList)({
+  })
+  const MyTab = styled(Tab)({
+      paddingRight:"130px",
+      paddingLeft:"130px"
+  })
 
 
-const Header = () =>{
-    return(
-        <>
-        <h1>header page</h1>
-        </>
-    )
+export default function Header() {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <MyBox sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <MyTabList onChange={handleChange} aria-label="lab API tabs example">
+            <MyTab label="소개공간" value="1" />
+            <MyTab label="여행지" value="2" />
+            <MyTab label="리뷰" value="3" />
+          </MyTabList>
+        </Box>
+        {/* <MyTabPanel value="1">SHOP</MyTabPanel>
+        <MyTabPanel value="2">TRIP</MyTabPanel>
+        <MyTabPanel value="3">COMMUNITY</MyTabPanel> */}
+      </TabContext>
+    </MyBox>
+  );
 }
-
-export default Header;
--- src/page/subMain/SubMainPageComponents.js
+++ src/page/subMain/SubMainPageComponents.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../common/Header";
+import ItemList from "../../common/ItemList";
 
 const SubMainPageComponents = () =>{
     return(
         <>
         <Header/>
+        <ItemList />
         </>
     )
 }
