9715ca726eb67f64609a95d373350d38c5936ea1
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-11-29 23:50:10 +0900

    TODO LIST

--- src/common/todo/TodoItem.js
+++ src/common/todo/TodoItem.js
@@ -1,5 +1,50 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { todoListState } from "../../Modules/Store";
 
-export default function TodoItem() {
-  return <div></div>;
+export default function TodoItem({ item }) {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  const index = todoList.findIndex((listItem) => listItem == item);
+
+  function replaceItemAtIndex(arr, index, newValue) {
+    return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+  }
+
+  function removeItemAtIndex(arr, index) {
+    return [...arr.slice(0, index), ...arr.slice(index + 1)];
+  }
+
+  const editItemText = ({ target: { value } }) => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      text: value,
+    });
+    setTodoList(newList);
+  };
+
+  const tooggleItemCompletion = () => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      isComplete: !item.isComplete,
+    });
+    setTodoList(newList);
+  };
+
+  const deleteItem = () => {
+    const newList = removeItemAtIndex(todoList, index);
+
+    setTodoList(newList);
+  };
+
+  return (
+    <div>
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input
+        type="checkbox"
+        checked={item.isComplete}
+        onChange={tooggleItemCompletion}
+      />
+      <button onClick={deleteItem}>X</button>
+    </div>
+  );
 }
