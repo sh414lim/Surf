a6c57c83611bc7f050a694b923ddc6770719ecfa
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-12-21 22:04:42 +0900

    demo date 연결

--- src/page/shop/ShopListPage.tsx
+++ src/page/shop/ShopListPage.tsx
@@ -1,12 +1,19 @@
+import axios from 'axios';
 import React, { useState, useEffect, useMemo } from 'react';
-import { fetchList } from '../../service/api';
+import { fetchList, getUser } from '../../service/api';
 
 function ShopListPage() {
   const [list, setList] = useState('');
 
   useEffect(() => {
-    const test = fetchList();
-    console.log(test);
+    const text = axios
+      .get('https://api.themoviedb.org/3/movie/550?api_key=13dce73d621a57563589d58029a15906')
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }, []);
 
   return <div>ShopListPage</div>;
--- src/service/api.tsx
+++ src/service/api.tsx
@@ -13,4 +13,31 @@ export function fetchList(): Promise<AxiosResponse<GetListResponse>> {
   return axios.get(url);
 }
 
+export async function getUser() {
+  // async, await을 사용하는 경우
+  try {
+    // GET 요청은 params에 실어 보냄
+    const response = await axios.get(
+      'https://api.themoviedb.org/3/movie/550?api_key=13dce73d621a57563589d58029a15906',
+      {
+        params: {
+          ID: 12345,
+        },
+      },
+    );
+
+    // 응답 결과(response)를 변수에 저장하거나.. 등 필요한 처리를 해 주면 된다.
+
+    await axios.get('/user?ID=12345'); // 위의 요청과 동일
+
+    var userId = 12345;
+    await axios.get(`/user?ID=${userId}`); // Backtick(`)을 이용해 이렇게 요청할 수도 있다.
+
+    console.log(response);
+  } catch (e) {
+    // 실패 시 처리
+    console.error(e);
+  }
+}
+
 fetchList().then(res => res.data.Message);
