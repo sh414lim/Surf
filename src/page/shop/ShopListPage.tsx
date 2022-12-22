f3f1610c6a9cdc81d86dba51bc4c9c6afe4ecca0
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-12-22 21:54:19 +0900

    demo api setting

--- src/page/shop/ShopListPage.tsx
+++ src/page/shop/ShopListPage.tsx
@@ -2,6 +2,15 @@ import axios from 'axios';
 import React, { useState, useEffect, useMemo } from 'react';
 import { fetchList, getUser } from '../../service/api';
 
+interface ListInterface {
+  id: number;
+  genre_ids: number[];
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  vote_count: number;
+}
+
 function ShopListPage() {
   const [list, setList] = useState([]);
 
@@ -17,7 +26,20 @@ function ShopListPage() {
   }, []);
 
   console.log(list);
-  return <>{list && list.map((props, index) => JSON.stringify(props))}</>;
+  return (
+    <>
+      {list &&
+        list.map(({ id, original_title, overview, poster_path, vote_count }: ListInterface, index: number) => (
+          <>
+            <img src={`https://image.tmdb.org/t/p/w200/${poster_path}`} />
+            <p>{original_title}</p>
+            <p>{id}</p>
+            <p>{overview}</p>
+            <p>{vote_count}</p>
+          </>
+        ))}
+    </>
+  );
 }
 
 // popular:()=>api.get("/3/movie/popular"),
