f0fe9fcb3bed0073164993f3a8015d69452f270a
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-12-22 17:06:46 +0900

    ts - setting

--- src/page/shop/ShopListPage.tsx
+++ src/page/shop/ShopListPage.tsx
@@ -3,20 +3,26 @@ import React, { useState, useEffect, useMemo } from 'react';
 import { fetchList, getUser } from '../../service/api';
 
 function ShopListPage() {
-  const [list, setList] = useState('');
+  const [list, setList] = useState([]);
 
   useEffect(() => {
     const text = axios
-      .get('https://api.themoviedb.org/3/movie/550?api_key=13dce73d621a57563589d58029a15906')
+      .get('https://api.themoviedb.org/3/movie/popular?api_key=13dce73d621a57563589d58029a15906')
       .then(res => {
-        console.log(res);
+        setList(res.data.results);
       })
       .catch(err => {
         console.error(err);
       });
   }, []);
 
-  return <div>ShopListPage</div>;
+  console.log(list);
+  return <>{list && list.map((props, index) => JSON.stringify(props))}</>;
 }
+
+// popular:()=>api.get("/3/movie/popular"),
+// toprated:()=>api.get("/3/movie/top_rated"),
+// upcoming:()=>api.get("/3/movie/upcoming"),
+// nowplaying:()=>api.get("/3/movie/now_playing"),
 
 export default ShopListPage;
