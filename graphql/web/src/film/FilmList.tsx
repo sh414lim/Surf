93b005a193e1f2a18f49bd083c845063024bef08
sh414lim <77113306+sh414lim@users.noreply.github.com>
2023-01-05 20:29:53 +0900

    graph ql ui

--- graphql/web/src/film/FilmList.tsx
+++ graphql/web/src/film/FilmList.tsx
@@ -1,4 +1,6 @@
+import { SimpleGrid, Skeleton, Box } from '@chakra-ui/react';
 import { useFilmsQuery } from '../generated/graphql';
+import FilmCard from './FilmCard';
 
 export default function FilmList(): JSX.Element {
   const { data, loading, error } = useFilmsQuery();
@@ -6,5 +8,16 @@ export default function FilmList(): JSX.Element {
   if (loading) return <p>...loading</p>;
   if (error) return <p>{error.message}</p>;
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return (
+    <SimpleGrid columns={[2, null, 3]} spacing={[2, null, 10]}>
+      {loading && new Array(6).fill(0).map(x => <Skeleton key={x} height="400px" />)}
+      {/* {!loading &&
+        data &&
+        data.films.map((film: any) => (
+          <Box key={film.id}>
+            <FilmCard film={film} />
+          </Box>
+        ))} */}
+    </SimpleGrid>
+  );
 }
